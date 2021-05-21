a812ad65c7ae6aaa9a7ca0db77ac6e1a89116b7b
leonel47 <waboleonel@gmail.com>
2021-05-21 11:30:59 -0400

    Start implementing model entities

---
+++ src/main/java/com/example/demo/model/Address.java
@@ -0,0 +1,12 @@
+package com.example.demo.model;
+
+public class Address {
+
+    private Long id;
+    private String country;
+    private String postalCode;
+    private String city;
+    private String town;
+    private String street;
+    private String phone;
+}
---
+++ src/main/java/com/example/demo/model/Client.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+
+public class Client {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
