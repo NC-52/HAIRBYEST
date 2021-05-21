0f2f1fe298f82887507c432aa78281dc1d288626
NC-52 <noutsawilliam88@gmail.com>
2021-05-21 10:17:18 +0100

    Initial Commit

---
+++ src/main/java/com/example/demo/HairbyestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairbyestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HairbyestApplication.class, args);
+    }
+
+}
